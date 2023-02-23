f299096d4f1e89cd6e335043edb6aa2949313bd8
angelaslinker <angela.r.slinker@gmail.com>
2023-02-23 09:30:47 -0700

    Initial Commit

---
+++ lookbook-app/lookbook_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  lookbook_appApp.swift
+//  lookbook-app
+//
+//  Created by Angela Slinker on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct lookbook_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
